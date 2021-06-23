2a6379a009c1deb6531ffe7a7633858c6cee6d8b
bhupesh7shakya <bhupesh7shakya@gmail>
2021-06-23 06:27:59 -0400

    "addied doctor layout page"

--- lib/screens/doctorlist.dart
+++ lib/screens/doctorlist.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:medico/components/components.dart';
+import 'package:medico/Data/data.dart';
 import 'package:medico/screens/shared/shared.dart';
 
 class DoctorList extends StatefulWidget {
@@ -46,6 +47,7 @@ class _DoctorListState extends State<DoctorList> {
         ],
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
             width: double.infinity,
@@ -54,19 +56,189 @@ class _DoctorListState extends State<DoctorList> {
               color: Color(0xffa3ffe7),
             ),
           ),
+          SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Text(
+              "Result showing Gerneal doctors",
+              style: TextStyle(
+                color: Components.componenet,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 25.0, vertical: 5),
+                  child: Container(
+                    height: 200,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 3,
+                          offset: Offset(3, 3),
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Expanded(
+                              flex: 3,
+                              child: Column(
+                                children: [
+                                  CircleAvatar(
+                                    radius: 28,
+                                    backgroundImage: NetworkImage(
+                                      doctorData[index].imgUrl,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Expanded(
+                              flex: 10,
+                              child: Padding(
+                                padding: const EdgeInsets.fromLTRB(5, 5, 0, 0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Stack(
+                                      children: [
+                                        Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              "Dr. John Doe",
+                                              style: TextStyle(
+                                                  fontSize: 22,
+                                                  color: Colors.black87,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                            Text(
+                                              "General Physician",
+                                              style: TextStyle(
+                                                  fontSize: 12,
+                                                  color: Colors.black54,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                            Text(
+                                              "MD- General Medicine, Lorem ipsum",
+                                              style: TextStyle(
+                                                  fontSize: 12,
+                                                  color: Colors.black54,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                          ],
+                                        ),
+                                        Positioned(
+                                          top: -9,
+                                          right: -17.5,
+                                          child: IconButton(
+                                              icon: Icon(Icons.share,
+                                                  size: 18,
+                                                  color: Components.componenet),
+                                              onPressed: null),
+                                        )
+                                      ],
+                                    ),
+                                    SizedBox(
+                                      height: 5,
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          "Rs 200",
+                                          style: TextStyle(
+                                              fontSize: 12,
+                                              color: Colors.black,
+                                              fontWeight: FontWeight.w900),
+                                        ),
+                                        Text(
+                                          "14 yrs of exp.",
+                                          style: TextStyle(
+                                              fontSize: 12,
+                                              color: Colors.black,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        TextButton.icon(
+                                          onPressed: null,
+                                          icon: Icon(Icons.favorite,
+                                              color: Colors.red),
+                                          label: Text(
+                                            "126",
+                                            style: TextStyle(
+                                                fontSize: 12,
+                                                color: Colors.black,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            LoginButton(
+                              oppositeTextColor: true,
+                              border: true,
+                              small: true,
+                              title: "Call",
+                              backgroundColor: null,
+                              function: () {
+                                print("call");
+                              },
+                            ),
+                            LoginButton(
+                              small: true,
+                              title: "Book",
+                              backgroundColor: Components.button,
+                              function: () {
+                                print("call");
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
         ],
       ),
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.fromLTRB(0, 60, 10, 0),
-        child: FloatingActionButton(
-          onPressed: () {},
-          backgroundColor: Components.componenet,
-          child: Icon(
-            Icons.settings,
-          ),
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.miniEndTop,
+      // floatingActionButton: Padding(
+      //   padding: const EdgeInsets.fromLTRB(0, 60, 10, 0),
+      //   child: FloatingActionButton(
+      //     onPressed: () {},
+      //     backgroundColor: Components.componenet,
+      //     child: Icon(
+      //       Icons.settings,
+      //     ),
+      //   ),
+      // ),
+      // floatingActionButtonLocation: FloatingActionButtonLocation.miniEndTop,
     );
   }
 }
--- lib/screens/shared/LoginButton.dart
+++ lib/screens/shared/LoginButton.dart
@@ -1,27 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:medico/components/components.dart';
 
 class LoginButton extends StatelessWidget {
   final String title;
   final LinearGradient backgroundColor;
   final Function function;
-
-  const LoginButton({Key key, this.title, this.backgroundColor, this.function})
+  final bool small;
+  final bool oppositeTextColor;
+  final bool border;
+  const LoginButton(
+      {Key key,
+      this.title,
+      this.backgroundColor,
+      this.function,
+      this.small = false,
+      this.oppositeTextColor = false,
+      this.border = false})
       : super(key: key);
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: function,
       child: Container(
-        width: MediaQuery.of(context).size.width * 0.9,
+        width: (small == true)
+            ? MediaQuery.of(context).size.width * 0.41
+            : MediaQuery.of(context).size.width * 0.9,
         height: 50,
         alignment: Alignment.center,
         decoration: BoxDecoration(
+          border: Border(
+
+              // need to add border this
+              ),
           borderRadius: BorderRadius.circular(50),
           gradient: backgroundColor,
         ),
         child: Text(
           title,
-          style: TextStyle(color: Colors.white,),
+          style: TextStyle(
+            color: (oppositeTextColor == true)
+                ? Components.componenet
+                : Colors.white,
+          ),
         ),
       ),
     );
